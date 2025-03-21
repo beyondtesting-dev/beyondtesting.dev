687e4d6c4f1eb2f03afa8ac52f9165299bca04bd
andreamancuso <and.mancuso@googlemail.com>
2025-03-21 22:09:17 +0100

    OK this should fix the build for real.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs"
+            to="/kb"
           >
             Learn More
           </Link>
