76e39d84e8c40555f5fb5fa7777f366fb786c75e
andreamancuso <and.mancuso@googlemail.com>
2025-03-22 11:05:55 +0000

    OK now we're talking

--- src/components/Mermaid.tsx
+++ src/components/Mermaid.tsx
@@ -1,28 +1,53 @@
 // components/Mermaid.tsx
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import mermaid from "mermaid";
+import Spinner from "./Spinner";
 
 mermaid.initialize({ startOnLoad: false });
 
 export default function Mermaid({ chart, id }: { chart: string; id: string }) {
+  const [loading, setLoading] = useState(true);
   const ref = useRef(null);
 
   useEffect(() => {
     if (ref.current) {
       if (ref.current) {
         ref.current.innerHTML = "";
-        mermaid
-          .render(id, chart)
-          .then((result) => {
-            ref.current!.innerHTML = result.svg;
-          })
-          .catch((err) => {
-            console.error("Mermaid render error:", err);
-            ref.current!.innerHTML = `<pre style="color:red;">Mermaid render error</pre>`;
-          });
+
+        requestIdleCallback(
+          () => {
+            mermaid
+              .render(id, chart)
+              .then((result) => {
+                ref.current!.innerHTML = result.svg;
+              })
+              .catch((err) => {
+                console.error("Mermaid render error:", err);
+                ref.current!.innerHTML = `<pre style="color:red;">Mermaid render error</pre>`;
+              })
+              .finally(() => {
+                setLoading(false);
+              });
+          },
+          { timeout: 2000 }
+        );
       }
     }
   }, [chart]);
 
-  return <div ref={ref} />;
+  const style = useMemo(
+    () => ({
+      minHeight: "700px",
+      opacity: loading ? 0 : 1,
+      transition: "opacity 0.3s ease-in-out",
+    }),
+    [loading]
+  );
+
+  return (
+    <>
+      {loading && <Spinner />}
+      <div ref={ref} style={style} />
+    </>
+  );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,4 @@
+// components/Spinner.tsx
+export default function Spinner() {
+  return <div className="spinner" role="status" aria-label="Loading diagram" />;
+}
